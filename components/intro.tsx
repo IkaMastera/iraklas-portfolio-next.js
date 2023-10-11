31e596f8c19a30ae4714935cea83e9f1c4f42786
Irakli <112602982+IkaMastera@users.noreply.github.com>
2023-10-11 17:30:43 +0400

    update: intro.tsx added buttons
    added Buttons and animations for those buttons

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,10 @@
 import Image from 'next/image'
 import React from 'react'
 import { motion } from 'framer-motion'
+import Link from 'next/link'
+import { BsArrowRight, BsLinkedin } from "react-icons/bs"
+import { HiDownload} from "react-icons/hi"
+import { FaGithubSquare } from 'react-icons/fa'
 
 export default function Intro() {
   return (
@@ -47,7 +51,6 @@ export default function Intro() {
        font-medium !leading-[1.5] sm:text-4xl'
        initial = {{ opacity: 0, y: 100}}
        animate = {{ opacity: 1, y: 0}}
-       
        >
         <span className='font-bold'>
           Hi, I'm Irakli Oboladze
@@ -62,6 +65,31 @@ export default function Intro() {
          {" "}
          <span className='font-bold underline'>Angular, React(Next.js)</span>
       </motion.p>
+
+      <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4
+      text-lg font-medium'
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.1,
+      }}
+      >
+        <Link href="#contact" className='bg-gray-900 text-white px-7 py-3 
+         flex items-center gap-2 rounded-full
+         outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'
+         >
+            Contact me here <BsArrowRight />
+        </Link>
+        <a href="" className='bg-white px-7 py-3 flex items-center gap-2 rounded-full'>
+            Download CV <HiDownload />
+        </a>
+        <a className='bg-white p-4 flex items-center text-gray-700 gap-2 rounded-full'>
+            <BsLinkedin />
+        </a>
+        <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem]'>
+            <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   )
 }
