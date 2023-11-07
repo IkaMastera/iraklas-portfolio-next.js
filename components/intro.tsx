a280ee05b99b468b74793f46e3d9f8cc13a8a8a2
Irakli <112602982+IkaMastera@users.noreply.github.com>
2023-11-07 13:53:26 +0400

    update: intro tsx file
    change p tag into a h1 tag makes more sense that way for search engine purposes

--- components/intro.tsx
+++ components/intro.tsx
@@ -47,7 +47,7 @@ export default function Intro() {
         </div>
       </div>
 
-      <motion.p className='mb-10 mt-4 px-4 text-2xl
+      <motion.h1 className='mb-10 mt-4 px-4 text-2xl
        font-medium !leading-[1.5] sm:text-4xl'
        initial = {{ opacity: 0, y: 100}}
        animate = {{ opacity: 1, y: 0}}
@@ -64,7 +64,7 @@ export default function Intro() {
          <span className='italic'>I enjoy Building Websites and software Using</span>
          {" "}
          <span className='font-bold underline'>Angular, React(Next.js)</span>
-      </motion.p>
+      </motion.h1>
 
       <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4
       text-lg font-medium'
