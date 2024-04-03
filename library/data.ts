91b8b8a04c0b3b1eed977de2cf5879db0f3172df
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-04-03 17:06:00 +0400

    add: third party components and skill component
    also made some changes in the data.ts file for my experiences

--- library/data.ts
+++ library/data.ts
@@ -39,9 +39,9 @@ export const experiencesData = [
     title: "Junior Front-End Developer",
     location: "Georgia, Tbilisi",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "I graduated after 6 months of studying. I immediately found a job as a front-end developer at a company called Redberry.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2019-2020",
   },
   {
     title: "Junior Back-End Developer",
@@ -49,15 +49,39 @@ export const experiencesData = [
     description:
       "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2020 - 2021",
+  },
+  {
+    title: "Front-End Developer Lecturer",
+    location: "Georgia, Tbilisi",
+    description:
+      "After 2 and a half years at Redberry I decided to get my skills to the next level at new companies and started to work as a front-end lecturer in digital academy for 1 month",
+    icon: React.createElement(FaReact),
+    date: "2023 - 2023",
   },
   {
     title: "Front-End Developer",
     location: "Georgia, Tbilisi",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "After my experiences as a lecturer and a full-stack developer at redberry i got offered a job at a company called UpGaming where i got more hang of languages like javascript and typescript",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2023 - 2023",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Georgia, Tbilisi",
+    description:
+      "Unfortunately UpGamings office was too far away from my house and that distance + the native language of javascript and making casino games wasnt my passion and i decided to shift my whole career to become a masterfull React.js developer and i started to work as a freelancer at an american company called Elgeo Corp",
+    icon: React.createElement(FaReact),
+    date: "2023 - 2024",
+  },
+  {
+    title: "BTU Masters Degree In Technology",
+    location: "Georgia, Tbilisi",
+    description:
+      "Right now I am Studying at university called BTU which is the best university for programming in Georgia I decided to study and get my skills to the next level to become the version of me that i've allways wanted",
+    icon: React.createElement(FaReact),
+    date: "2023 - 2024",
   },
 ] as const;
 
