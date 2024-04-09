39992d02fe31204afba875020bf77567bcaf4d5d
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-04-09 16:15:01 +0400

    add: experience component

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
 import {
@@ -5,13 +7,39 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/library/data";
 
 export default function Experience() {
   return (
     <section id="experience">
       <SectionHeading>My Experience</SectionHeading>
       <VerticalTimeline>
-        <VerticalTimelineElement></VerticalTimelineElement>
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+              contentStyle={{
+                background: "#f3f4f6",
+                boxShadow: "none",
+                border: "1px solid rgba(0, 0, 0, 0.05)",
+                textAlign: "left",
+                padding: "1.3rem 2rem",
+              }}
+              contentArrowStyle={{
+                borderRight: "0.4rem solid #9ca3af",
+              }}
+              date={item.date}
+              icon={item.icon}
+              iconStyle={{
+                background: "white",
+                fontSize: "1.5rem",
+              }}
+            >
+              <h3>{item.title}</h3>
+              <p>{item.location}</p>
+              <p>{item.description}</p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
       </VerticalTimeline>
     </section>
   );
