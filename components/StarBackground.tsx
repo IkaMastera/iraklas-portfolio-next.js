6ad7f67b3fc95abd74d1ca6f9486b185fcdca5ef
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-04-24 13:36:12 +0400

    fix: bug with star Canvas
    the star canvas was overlaying on top of everything and because of that nothing was clickable anymore also you should try the dark mode with this its really pretty :smile:

--- components/StarBackground.tsx
+++ components/StarBackground.tsx
@@ -50,7 +50,7 @@ const StarBackground = (props: any) => {
 
 const StarsCanvas = () => {
   return (
-    <div className="w-full h-auto fixed inset-0 z-[20]">
+    <div className="w-full h-full fixed inset-0 z-[-1]">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <StarBackground />
